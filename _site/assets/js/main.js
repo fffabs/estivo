5473a77e428168fca22de963802ef2a33b98a234
Max White <mushishi78@gmail.com>
2015-09-07 21:30:53 +0100

    gitignore _site

--- _site/assets/js/main.js
+++
@@ -1,6 +0,0 @@
-// grab an element
-var myElement = document.querySelector("header-container");
-// construct an instance of Headroom, passing the element
-var headroom  = new Headroom(myElement);
-// initialise
-headroom.init();
